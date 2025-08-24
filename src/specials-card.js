dfcb5f9f18425467a415067a7d21f032924659c5
Abhijit Ghosh <a7ghosh@gmail.com>
2025-08-24 23:48:17 +0530

    Designed the card component

--- src/specials-card.js
+++ src/specials-card.js
@@ -5,15 +5,17 @@ export default function SpecialsCard({ title, price, description, image }) {
   return (
     <div className="specials-card">
       <img src={image} alt={title} />
-      <div className="name-price">
-        <p>{title}</p>
-        <p>{price}</p>
-      </div>
-      <p>{description}</p>
-      <div className="order-button">
-        <button>Order a delivery</button>
-        <img src={scooter} alt="scooter_icon" />
-      </div>
+        <div className="card-text">
+          <div className="name-price">
+            <p>{title}</p>
+            <p>{price}</p>
+          </div>
+          <p className="description">{description}</p>
+          <div className="order-button">
+            <button>Order a delivery</button>
+            <img src={scooter} alt="scooter_icon" />
+          </div>
+        </div>
     </div>
   );
 }
--- src/specials.js
+++ src/specials.js
@@ -3,20 +3,20 @@ import {specials} from './specials-data';
 import SpecialsCard from './specials-card';
 export default function Specials() {
   return (
-    <section>
-      <h2>Specials</h2>
-      <button>Online Menu</button>
-      {specials.map((special, index) => {
-        return (
-          <SpecialsCard
-            key={index}
-            title={special.title}
-            price={`$${special.price.toFixed(2)}`}
-            description={special.description}
-            image={special.image}
-          />
-        )
-      })}
+    <section className="specials-section">
+        <h2>Specials</h2>
+        <button>Online Menu</button>
+        {specials.map((special, index) => {
+          return (
+            <SpecialsCard
+              key={index}
+              title={special.title}
+              price={`$${special.price.toFixed(2)}`}
+              description={special.description}
+              image={special.image}
+            />
+          )
+        })}
     </section>
   );
 }
