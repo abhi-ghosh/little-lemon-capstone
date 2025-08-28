45e5805aff24795ca5937478264ddc43105a4b24
Abhijit Ghosh <a7ghosh@gmail.com>
2025-08-29 02:09:20 +0530

    Made the Nav mobile compatible

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {navLinks} from './link-content';
 import Links from './links';
 import logo from './assets/logo.png';
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const click = () => {
+    isOpen?console.log("Menu Closed"):console.log("Menu Opened");
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="nav-container">
       <div className="nav">
         <img src={logo} alt="Little Lemon Logo" />
-        <Links className = "nav-links" links={navLinks}/>
+
+        {/* Hamburger button */}
+        <div
+          className={`hamburger ${isOpen ? "active" : ""}`}
+          onClick={click}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Add conditional class here */}
+        <Links
+          className={`nav-links ${isOpen ? "active" : ""}`}
+          links={navLinks}
+        />
       </div>
     </nav>
   );
