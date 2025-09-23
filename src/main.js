8fe8a6b8cc11aa77638559e7691a2cd5f50b2aa7
Abhijit Ghosh <a7ghosh@gmail.com>
2025-09-24 02:48:39 +0530

    Made the reviews section

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './hero';
 import Specials from './specials';
+import RatingCard from './rating-card';
 export default function Main() {
   return (
     <main>
       <Hero/>
       <Specials/>
+      <RatingCard/>
     </main>
   );
 }
--- src/rating-data.js
+++ src/rating-data.js
@@ -1,3 +1,10 @@
-export const rating = [
- 
+import barry from './assets/barry.jpg';
+import anne from './assets/anne.jpg';
+import bruce from './assets/bruce.jpg';
+import peter from './assets/peter.jpg';
+export const ratings = [
+ {star:5,img:barry,name:"Barry Allen",comment:"Amazing food!!"},
+ {star:4,img:peter,name:"Peter Parker",comment:"Great service, 10/10 food"},
+ {star:5,img:anne,name:"Anne Talia",comment:"Love it!"},
+ {star:5,img:bruce,name:"Bruce Wayne",comment:"Perfect in every way!"}
 ]
